a1a9dad7b003fef71563b5dceaae40133ec2af23
Viv921 <149313944+Viv921@users.noreply.github.com>
2024-03-03 12:44:32 +0530

    Create statAllRounder.py

---
+++ Project/statAllRounder.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import os 
+import csv
+
+file_path = os.path.join(os.getcwd(), "data")
+
+
+def load_data(filename, file_path=file_path):
+    csv_path = os.path.join(file_path, filename)
+    return pd.read_csv(csv_path)
+
+
+allround = pd.read_csv("Project\\data\\finalAllRounder.csv")
+
+def prediction(allround, name):
+    score = 0
+    for i in range(1, 6, 1):
+        
+        score += i * allround.at[i-1, name]
+    score /= 15
+    return score
+
+
+
+def predictedAllRound():
+    predictedAllRounder = []
+    predictedAllRounder.append(prediction(allround, "Shakib"))
+    predictedAllRounder.append(prediction(allround, "Rashid"))
+    predictedAllRounder.append(prediction(allround, "Maxwell"))
+    predictedAllRounder.append(prediction(allround, "Jadeja"))
+    predictedAllRounder.append(prediction(allround, "Jansen"))
+    predictedAllRounder.append(prediction(allround, "Rachin"))
+    predictedAllRounder.append(prediction(allround, "Hasranga"))
+    predictedAllRounder.append(prediction(allround, "Pandya"))
+    predictedAllRounder.append(prediction(allround, "Holder"))
+    predictedAllRounder.append(prediction(allround, "Zampa"))
+    predictedAllRounder.append(prediction(allround, "Head"))
+    predictedAllRounder.append(prediction(allround, "Green"))
+
+    player_namePred = ['Shakib','Rashid','Maxwell','Jadeja','Jansen','Rachin','Hasranga','Pandya','Holder','Zampa','Head','Green']
+    dicPredAll = {'Player': player_namePred, 'Match 6': predictedAllRounder}
+    
+    df_predAll = pd.DataFrame(dicPredAll, index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    return df_predAll
+
+
