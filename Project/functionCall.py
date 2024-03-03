0d8ff72beb45ff949450fb1c3e9e719845f0dd78
Viv921 <149313944+Viv921@users.noreply.github.com>
2024-03-03 12:44:24 +0530

    Create functionCall.py

---
+++ Project/functionCall.py
@@ -0,0 +1,15 @@
+from topAllRounder import *
+from topBatsman import *
+from topBowlers import *
+from statAllRounder import predictedAllRound
+from statBatsman import predictedBat
+from statBowler import predictedBall
+
+topBat()
+topAllRound()
+topBall()
+predictedBat()
+predictedAllRound()
+predictedBall()
+
+
